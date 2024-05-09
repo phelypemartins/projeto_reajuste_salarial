30af5e08ea09bd54716abc1aa8f4618f4dea7b45
unknown <phelypemartins@gmail.com>
2024-05-08 22:19:17 -0300

    Ajuste no bloco de indentação/redução das linhas de código

--- reajuste.py
+++ reajuste.py
@@ -1,45 +1,24 @@
-#Programa realizado para calcular reajuste dos salários dos funcionários
-
 salario = float(input('Digite o salário do colaborador: '))
+#reajuste = float(input('Digite o reajuste: '))
 
 
-
-if salario >= 1500:
-    reajuste = 0.05
-    aumento = salario*reajuste
-    novo_salario = salario + aumento
-
-    print(f'O salário antes do reajuste: {salario}')
-    print(f'O percentual de aumento aplicado foi de {reajuste}')
-    print(f'O valor do aumento foi de {aumento}')
-    print(f'O novo salário, após o aumento é de {novo_salario}')
-
-elif salario < 1500 and salario > 700:
-    reajuste = 0.1
-    aumento = salario*0.1
-    novo_salario = salario + aumento
-
-    print(f'O salário antes do reajuste: {salario}')
-    print(f'O percentual de aumento aplicado foi de {reajuste}')
-    print(f'O valor do aumento foi de {aumento}')
-    print(f'O novo salário, após o aumento é de {novo_salario}')
-
-elif salario < 700 and salario > 280:
-    reajuste = 0.15
-    aumento = salario*0.15
-    novo_salario = salario + aumento
+if salario <= 280:
+    reajuste = 0.2
     
-    print(f'O salário antes do reajuste: {salario}')
-    print(f'O percentual de aumento aplicado foi de {reajuste}')
-    print(f'O valor do aumento foi de {aumento}')
-    print(f'O novo salário, após o aumento é de {novo_salario}')   
+elif salario <= 700:
+    reajuste = 0.15
+    
+elif salario < 1500:
+    reajuste = 0.1
 
 else:
-    aumento = salario*0.20
-    reajuste = 0.2
-    novo_salario = salario + aumento
+    reajuste = 0.05
     
-    print(f'O salário antes do reajuste: {salario}')
-    print(f'O percentual de aumento aplicado foi de {reajuste}')
-    print(f'O valor do aumento foi de {aumento}')
-    print(f'O novo salário, após o aumento é de {novo_salario}')
+
+aumento = salario*reajuste
+novo_salario = salario + aumento
+    
+print(f'O salário antes do reajuste: {salario}')
+print(f'O percentual de aumento aplicado foi de {reajuste}%')
+print(f'O valor do aumento foi de {aumento}')
+print(f'O novo salário, após o aumento é de {novo_salario}')
